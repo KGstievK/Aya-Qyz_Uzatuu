f6c8ec40195f166d7d44b779fc0861f9e92d53c3
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-07-17 02:56:12 +0600

    Aijan and Kutman

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,13 +1,11 @@
 "use client"
-import { SetStateAction } from "react"
 import Dad from "./Dad/Dad"
 import FormGuest from "./FormGuest/FormGuest"
 import Gps from "./Gps/Gps"
 import Map from "./Map/Map"
 import NikhahSection from "./Nikhah/NikhahSection"
-import Timer from "./Timer/Timer"
-import Welcome from "./Welcome/Welcome"
 import Time from "./Time/Time"
+import Timer from "./Timer/Timer"
 
 const Home = () => {
   return (
