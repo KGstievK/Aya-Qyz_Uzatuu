e475397f7dc561f4c618bf72c677e4122044bf79
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-07-17 23:28:01 +0600

    Aijan Kutman1

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'static/fonts/',
-        },
-      },
-    });
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
