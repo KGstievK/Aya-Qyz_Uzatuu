142f7f5364b18c796c7f279aa3f8dc0acb64fc2c
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-08-14 11:42:16 +0600

    update

--- src/components/pages/Nikhah/NikhahSection.tsx
+++ src/components/pages/Nikhah/NikhahSection.tsx
@@ -1,50 +1,53 @@
-import Image from 'next/image'
-import scss from './NikhahSection.module.scss'
-import floral1 from "@/assets/Vector.svg"
-import back from "@/assets/back.jpg"
-import { Oswald, } from "next/font/google";
-
-
+import Image from "next/image";
+import scss from "./NikhahSection.module.scss";
+import floral1 from "@/assets/Vector.svg";
+import back from "@/assets/back.jpg";
+import { Oswald } from "next/font/google";
 
 const oswald = Oswald({ subsets: ["latin"] });
 
 const NikhahSection = () => {
-
   return (
     <section className={scss.NikhahSection}>
       <div className="container">
-        <Image className={scss.back} src={back} alt='' />
-          <h1>Wedding Day</h1>
+        <Image className={scss.back} src={back} alt="" />
+        <h1>Wedding Day</h1>
         <div className={scss.content}>
           <div className={scss.name}>
             <div className={scss.we}>
-              <Image className={scss.floral1} src={floral1} alt=''/>
+              <Image className={scss.floral1} src={floral1} alt="" />
               <div className={scss.date}>
                 <h2 className={oswald.className}>ОКТЯБРЯ</h2>
                 <div className={scss.day}>
-                <h3 className={oswald.className}>5</h3>
-                <p className={oswald.className}>2024</p>
+                  <h3 className={oswald.className}>4</h3>
+                  <p className={oswald.className}>2024</p>
                 </div>
                 <h2 className={oswald.className}>СУББОТА</h2>
+              </div>
+              <div className={scss.time}>
+                <h4 className={oswald.className}>18:00</h4>
               </div>
             </div>
           </div>
           <div className={scss.name}>
-              <div className={scss.we}>
-                <div className={scss.dateCopy}>
-                  <h2 className={oswald.className}>ОКТЯБРЯ</h2>
-                  <div className={scss.day}>
-                  <h3 className={oswald.className}>5</h3>
+            <div className={scss.we}>
+              <div className={scss.dateCopy}>
+                <h2 className={oswald.className}>ОКТЯБРЯ</h2>
+                <div className={scss.day}>
+                  <h3 className={oswald.className}>4</h3>
                   <p className={oswald.className}>2024</p>
-                  </div>
-                  <h2 className={oswald.className}>СУББОТА</h2>
                 </div>
+                <h2 className={oswald.className}>СУББОТА</h2>
               </div>
+              <div className={scss.timeCopy}>
+                <h4 className={oswald.className}>18:00</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default NikhahSection
+export default NikhahSection;
