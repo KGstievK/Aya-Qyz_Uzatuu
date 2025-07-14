2e6bc94d80579b8c31a5001787c6509188cdb4b3
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2025-07-14 11:08:49 +0600

    Исправлено

--- src/components/pages/Timer/Timer.tsx
+++ src/components/pages/Timer/Timer.tsx
@@ -21,7 +21,7 @@ const Timer: React.FC = () => {
               ТОЮНА АРНАЛГАН АК ДАСТОРКОНУБУЗДУН КАДЫРЛУУ КОНОГУ БОЛУУГА
               ЧАКЫРАБЫЗ
             </p>
-            <p className={oswald.className}>Саат 13:00</p>
+            <p className={oswald.className}>Саат 12:00</p>
             <h1>Той ээси: Арген</h1>
           </div>
           <div className={scss.titlee}>
@@ -33,7 +33,7 @@ const Timer: React.FC = () => {
             <p className={oswald.className}>
               УВАЖАЕМЫЕ ГОСТИ! МЫ ПРИГЛАШАЕМ ВАС НА ТОРЖЕСТВЕННЫЙ БАНКЕТ, ПОСВЯЩЕННЫЙ ПРОВОДАМ НАШЕЙ ЛЮБИМОЙ ДОЧЕРИ А-Я
             </p>
-            <p className={oswald.className}>В 13:00</p>
+            <p className={oswald.className}>В 12:00</p>
             {/* <p className={oswald.className}>
               Будем рады пригласить вас стать почётными гостями торжества,
               посвящённого бракосочетания
